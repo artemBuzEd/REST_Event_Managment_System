6596dd53b91e82c660b564b60574352d2b3dee4a
artemBuzEd <buziak.artem@chnu.edu.ua>
2025-06-14 19:51:44 +0300

    Added Unit Of Work implementation

---
+++ EMS.DAL.EF/UOW/Contract/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+using EMS.DAL.EF.Repositories.Contracts;
+
+namespace EMS.DAL.EF.UOW.Contract;
+
+public interface IUnitOfWork 
+{
+    IEMSAttendeeRepository Attendees { get; }
+    IEMSEventRepository Events { get; }
+    IEMSEventCategoryRepository EventCategories { get; }
+    IEMSOrganizerRepository Organizers { get; }
+    IEMSVenueRepository Venues { get; }
+    IEMSRegistrationRepository Registrations { get; }
+    
+    Task BeginTransactionAsync(CancellationToken cancellationToken);
+    Task CommitTransactionAsync(CancellationToken cancellationToken);
+    Task RollbackTransactionAsync(CancellationToken cancellationToken);
+    Task<int> CompleteAsync(CancellationToken cancellationToken);
+    void Dispose();
+}
---
+++ EMS.DAL.EF/UOW/UnitOfWork.cs
@@ -0,0 +1,63 @@
+using EMS.DAL.EF.Data;
+using EMS.DAL.EF.Repositories.Contracts;
+using EMS.DAL.EF.UOW.Contract;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace EMS.DAL.EF.UOW;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly EMSManagmentDbContext _context;
+    private IDbContextTransaction _transaction;
+    
+    public IEMSAttendeeRepository Attendees { get; }
+    public IEMSEventRepository Events { get; }
+    public IEMSEventCategoryRepository EventCategories { get; }
+    public IEMSOrganizerRepository Organizers { get; }
+    public IEMSRegistrationRepository Registrations { get; }
+    public IEMSVenueRepository Venues { get; }
+
+    public UnitOfWork(EMSManagmentDbContext context, 
+        IEMSAttendeeRepository attendees, 
+        IEMSEventRepository events, 
+        IEMSEventCategoryRepository eventCategories, 
+        IEMSOrganizerRepository organizers, 
+        IEMSRegistrationRepository registrations, 
+        IEMSVenueRepository venues)
+    {
+        _context = context;
+        Attendees = attendees;
+        Events = events;
+        EventCategories = eventCategories;
+        Organizers = organizers;
+        Registrations = registrations;
+        Venues = venues;
+    }
+
+    public async Task BeginTransactionAsync(CancellationToken cancellationToken = default)
+    {
+        _transaction = await _context.Database.BeginTransactionAsync(cancellationToken);
+    }
+
+    public async Task CommitTransactionAsync(CancellationToken cancellationToken = default)
+    {
+        await _transaction.CommitAsync(cancellationToken);
+    }
+
+    public async Task RollbackTransactionAsync(CancellationToken cancellationToken = default)
+    {
+        await _transaction.RollbackAsync(cancellationToken);
+    }
+
+    public async Task<int> CompleteAsync(CancellationToken cancellationToken = default)
+    {
+        return await _context.SaveChangesAsync(cancellationToken);
+    }
+
+    public void Dispose()
+    {
+        _transaction?.Dispose();
+        
+        _context?.Dispose();
+    }
+}
